e2fec70397a8fc1b6b75bb8a900bad7d863c49ab
Enrique Almeida <ealmeida@example.com>
2025-11-19 00:13:04 -0500

    fixed code

--- static/assets/dist/js/precipitation_concentration_index_by_pluviometer.js
+++ static/assets/dist/js/precipitation_concentration_index_by_pluviometer.js
@@ -5,7 +5,7 @@ const csrfToken = document.cookie
   ?.split("=")[1];
 axios.defaults.headers.common["X-CSRFToken"] = csrfToken;
 // url del endpoint principal
-const url = `/business-gestion/precipitation-concentration-index-by-pluviometer/?pluviometer=${pluviometer_id}`;
+const url = `/business-gestion/precipitation-concentration-index-by-pluviometer/`;
 console.log(url)
 $(function () {
   bsCustomFileInput.init();
