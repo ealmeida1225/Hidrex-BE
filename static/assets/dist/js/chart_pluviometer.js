273e1268b1453ceed00863fd1345cab5c5ef3e18
Enrique Almeida <ealmeida@example.com>
2025-11-25 13:34:40 -0500

    created file structure

---
+++ static/assets/dist/js/chart_pluviometer.js
@@ -0,0 +1,372 @@
+// Variable con el token
+const csrfToken = document.cookie
+  .split(";")
+  .find((c) => c.trim().startsWith("csrftoken="))
+  ?.split("=")[1];
+axios.defaults.headers.common["X-CSRFToken"] = csrfToken;
+// url del endpoint principal
+// const url = "/business-gestion/pluviometer/";
+const url = "/business-gestion/pluviometer/";
+
+$(function () {
+  bsCustomFileInput.init();
+  poblarListas();
+});
+function navigateToURL(url, id) {
+    if (!url || !id) {
+        console.error("URL o ID no proporcionados a navigateToArea.");
+        return;
+    }
+    localStorage.setItem('pluviometer_id', id);
+    window.location.href = url;
+}
+
+$(document).ready(function () {
+  $("table")
+    .addClass("table table-hover")
+    .DataTable({
+      dom: '<"top"l>Bfrtip',
+      buttons: [
+        {
+          text: "Crear",
+          className: " btn btn-primary btn-info",
+          action: function (e, dt, node, config) {
+            $("#modal-create-pluviometer").modal("show");
+          },
+        },
+        {
+          extend: "excel",
+          text: "Excel",
+        },
+        {
+          extend: "pdf",
+          text: "PDF",
+        },
+        {
+          extend: "print",
+          text: "Print",
+        },
+      ],
+      //Adding server-side processing
+      serverSide: true,
+      search: {
+        return: true,
+      },
+      processing: true,
+      ajax: function (data, callback, settings) {
+        dir = "";
+
+        if (data.order[0].dir == "desc") {
+          dir = "-";
+        }
+
+        axios
+          .get(`${url}`, {
+            params: {
+              page_size: data.length,
+              page: data.start / data.length + 1,
+              search: data.search.value,
+              ordering: dir + data.columns[data.order[0].column].data,
+            },
+          })
+          .then((res) => {
+            callback({
+              recordsTotal: res.data.count,
+              recordsFiltered: res.data.count,
+              data: res.data.results,
+            });
+          })
+          .catch((error) => {
+            alert(error);
+          });
+      },
+      columns: [
+        { data: "name", title: "Nombre" },
+        { data: "pluviometer_type_name", title: "Tipo" },
+        { data: "station_name", title: "Nombre de la estación" },
+        { data: "lat", title: "Latitud" },
+        { data: "lon", title: "Longitud" },
+        { data: "msnm", title: "Metros sobre nivel del mar" },
+
+        {
+          data: "",
+          title: "Acciones",
+          render: (data, type, row) => {
+            // 1. Verificamos si lat o lon están vacíos, son nulos o undefined
+            const isMapDisabled = !row.lat || !row.lon;
+
+            // 2. Construimos el HTML del botón del mapa de forma condicional
+            let mapButtonHtml;
+            if (isMapDisabled) {
+              // Si no hay coordenadas, el botón se crea deshabilitado
+              mapButtonHtml = `<button type="button" title="Sin coordenadas" class="btn bg-olive disabled" disabled>
+                                  <i class="fas fa-map-marker-alt"></i>
+                              </button>`;
+            } else {
+              // Si hay coordenadas, el botón se crea normalmente
+              mapButtonHtml = `<button type="button" title="Ver en mapa" class="btn bg-olive" onclick="navigateToURL('../map-pluviometer-page/', '${row.id}')">
+                                  <i class="fas fa-map-marker-alt"></i>
+                              </button>`;
+            }
+
+            // 3. Devolvemos el HTML completo del grupo de botones
+            return `<div class="btn-group"> 
+                        ${mapButtonHtml}
+                      <button type="button" title="Estadísticas por mes" 
+                          class="btn bg-olive" 
+                          onclick="navigateToURL('../month-statistics-page/', '${row.id}')">
+                          <i class="fas fa-star"></i>
+                      </button>                          
+                      <button type="button" 
+                        title="Estadísticas por año" 
+                        class="btn bg-olive" 
+                        onclick="navigateToURL('../year-statistics-page/', '${row.id}')">
+                        <i class="fas fa-star"></i>
+                      </button>                      
+                      <button type="button" 
+                        title="CI con datos históricos" 
+                        class="btn bg-olive" 
+                        onclick="navigateToURL('../precipitation-concentration-index-by-pluviometer-detail-page/', '${row.id}')">
+                        <i class="fas fa-sticky-note"></i>
+                      </button>
+                      <button type="button" 
+                        title="CI con datos históricos por mes" 
+                        class="btn bg-olive" 
+                        onclick="navigateToURL('../precipitation-concentration-index-monthly-by-pluviometer-page/', '${row.id}')">
+                        <i class="fas fa-stamp"></i>
+                      </button>
+                      <button type="button" 
+                        title="edit" 
+                        class="btn bg-olive active" 
+                        data-toggle="modal" 
+                        data-target="#modal-create-pluviometer" 
+                        data-id="${row.id}" 
+                        data-type="edit" data-name="${row.name}" id="${row.id}" >
+                        <i class="fas fa-edit"></i>
+                      </button>
+                      <button type="button" 
+                        title="delete" 
+                        class="btn bg-olive" 
+                        onclick="function_delete('${row.id}','${row.name}')" >
+                        <i class="fas fa-trash"></i>
+                      </button>                                        
+                    </div>`;
+          },
+        },
+      ],
+      //  esto es para truncar el texto de las celdas
+      columnDefs: [],
+    });
+});
+
+let selected_id;
+
+$("#modal-create-pluviometer").on("hide.bs.modal", (event) => {
+  const form = event.currentTarget.querySelector("form");
+  form.reset();
+  edit_pluviometer = false;
+  const elements = [...form.elements];
+  elements.forEach((elem) => elem.classList.remove("is-invalid"));
+});
+
+let edit_pluviometer = false;
+$("#modal-create-pluviometer").on("show.bs.modal", function (event) {
+  var button = $(event.relatedTarget); // Button that triggered the modal
+
+  var modal = $(this);
+  if (button.data("type") == "edit") {
+    var dataName = button.data("name"); // Extract info from data-* attributes
+    selected_id = button.data("id"); // Extract info from data-* attributes
+    edit_pluviometer = true;
+
+    modal.find(".modal-title").text("Editar " + dataName);
+
+    // Realizar la petición con Axios
+    axios
+      .get(`${url}${selected_id}/`)
+      .then(function (response) {
+        // Recibir la respuesta
+        const selected_pluviometer = response.data;
+        console.log(form.elements);
+
+        form.elements.name.value = selected_pluviometer.name;
+        form.elements.station_name.value = selected_pluviometer.station_name;
+        form.elements.pluviometer_type.value = selected_pluviometer.pluviometer_type;
+        form.elements.lat.value = selected_pluviometer.lat;
+        form.elements.lon.value = selected_pluviometer.lon;
+        form.elements.msnm.value = selected_pluviometer.msnm;
+
+        // document.getElementById("selectbrands").select= models.brand;
+      })
+      .catch(function (error) {});
+  } else {
+    modal.find(".modal-title").text("Crear");
+  }
+});
+
+// form validator
+$(function () {
+  $.validator.setDefaults({
+    language: "es",
+    submitHandler: function () {
+      // alert("Form successful submitted!");
+    },
+  });
+
+  $("#form-create-pluviometer").validate({
+    rules: {
+      name: {
+        required: true,
+      },
+    },
+    submitHandler: function (form) {},
+
+    messages: {},
+    errorElement: "span",
+    errorPlacement: function (error, element) {
+      error.addClass("invalid-feedback");
+      element.closest(".form-group").append(error);
+    },
+    highlight: function (element, errorClass, validClass) {
+      $(element).addClass("is-invalid");
+    },
+    unhighlight: function (element, errorClass, validClass) {
+      $(element).removeClass("is-invalid");
+    },
+  });
+});
+
+// crear Modelo
+
+let form = document.getElementById("form-create-pluviometer");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  var table = $("#tabla-de-Datos").DataTable();
+  const csrfToken = document.cookie
+    .split(";")
+    .find((c) => c.trim().startsWith("csrftoken="))
+    ?.split("=")[1];
+  axios.defaults.headers.common["X-CSRFToken"] = csrfToken;
+  let data = new FormData();
+  data.append("name", document.getElementById("name").value);
+  data.append("station_name", document.getElementById("station_name").value);
+  data.append("lat", document.getElementById("lat").value);
+  data.append("lon", document.getElementById("lon").value);
+  data.append("pluviometer_type", document.getElementById("pluviometer_type").value);
+  data.append("msnm", document.getElementById("msnm").value);
+
+  if (edit_pluviometer) {
+    axios
+      .patch(`${url}` + selected_id + "/", data)
+      .then((response) => {
+        if (response.status === 200) {
+          $("#modal-create-pluviometer").modal("hide");
+          Swal.fire({
+            icon: "success",
+            title: "Actualizado con éxito  ",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          table.ajax.reload();
+
+          edit_pluviometer = false;
+        }
+      })
+      .catch((error) => {
+        let dict = error.response.data;
+        let textError = "Revise los siguientes campos: ";
+        for (const key in dict) {
+          textError = textError + ", " + key;
+        }
+
+        Swal.fire({
+          icon: "error",
+          title: "Error al crear",
+          text: textError,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      });
+  } else {
+    axios
+      .post(`${url}`, data)
+      .then((response) => {
+        if (response.status === 201) {
+          Swal.fire({
+            icon: "success",
+            title: "Pluviómetro creado con éxito",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          table.ajax.reload();
+          $("#modal-create-pluviometer").modal("hide");
+        }
+      })
+      .catch((error) => {
+        let dict = error.response.data;
+        let textError = "Revise los siguientes campos: ";
+        for (const key in dict) {
+          textError = textError + ", " + key;
+        }
+
+        Swal.fire({
+          icon: "error",
+          title: "Error al crear",
+          text: textError,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      });
+  }
+});
+
+function poblarListas() {
+  var $pluviometer_types = document.getElementById("pluviometer_type");
+
+  axios.get("/business-gestion/pluviometer-type/").then(function (response) {
+    response.data.results.forEach(function (element) {
+      var option = new Option(element.name, element.id);
+      $pluviometer_types.add(option);
+    });
+  });
+}
+
+function function_delete(id, name) {
+  const table = $("#tabla-de-Datos").DataTable();
+  Swal.fire({
+    title: "Eliminar",
+    text: `Esta seguro que desea eliminar el elemento ${name}?`,
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Si, Eliminar",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      axios.defaults.headers.common["X-CSRFToken"] = csrfToken;
+      axios
+        .delete(`${url}${id}/`)
+        .then((response) => {
+          if (response.status === 204) {
+            table.row(`#${id}`).remove().draw();
+            Swal.fire({
+              icon: "success",
+              title: "Eliminar Elemento",
+              text: "Elemento eliminado satisfactoriamente ",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+        })
+        .catch((error) => {
+          Swal.fire({
+            icon: "error",
+            title: "Error eliminando elemento",
+            text: error.response.data.detail,
+            showConfirmButton: false,
+            timer: 3000,
+          });
+        });
+    }
+  });
+}
